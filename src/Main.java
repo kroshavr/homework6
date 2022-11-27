c5557b2db932ffc910a22b8c56e8d1f719400ddb
Костя <kroshavr@mail.ru>
2022-11-27 12:14:42 +0500

    Мастер

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+//Блок 1
+    //Задание
+        for (int i = 1; i <= 10; i++)
+        System.out.println(i);
+    //Задание 2
+        for (int a = 10; a >= 1; a--)
+            System.out.println(a);
+    //Задание 3
+        for (int b = 0; b <= 17; b = b + 2)
+            System.out.println(b);
+    //Задание 4
+        for (int c = 10; c >= -10; c--)
+            System.out.println(c);
+//Блок 2
+    //Задание 1
+        for (int year = 1904; year <= 2096; year = year + 4)
+            System.out.println(year);
+    //Задание 2
+        for (int d = 7; d <= 98; d = d + 7)
+            System.out.println(d);
+    //Задание 3
+        for (int e = 1; e <= 512; e = e * 2)
+            System.out.println(e);
+
+    }
+
+}
