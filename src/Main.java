4ebbec1a82b1070e3b511d96fc9921d73d21bfa1
Костя <kroshavr@mail.ru>
2022-11-27 12:55:08 +0500

    Вторая ветка

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
 //Блок 1
     //Задание
         for (int i = 1; i <= 10; i++)
@@ -17,14 +16,28 @@ public class Main {
 //Блок 2
     //Задание 1
         for (int year = 1904; year <= 2096; year = year + 4)
-            System.out.println(year);
+            System.out.println(year + " год является високосным");
     //Задание 2
         for (int d = 7; d <= 98; d = d + 7)
             System.out.println(d);
     //Задание 3
         for (int e = 1; e <= 512; e = e * 2)
             System.out.println(e);
-
+//Блок 3
+    //Задание 1
+        int salary = 29000;
+        int total = 0;
+        for (int f = 1; f <= 12; f++) {
+            total = total + salary;
+            System.out.println("Месяц " + f + ", сумма накоплений равна " + total + " рублей");
+        }
+    //Задание 2
+        int salary2 = 29000;
+        int total2 = 0;
+        for (int s = 1; s <= 12; s++) {
+            total2 = total2 + total2 / 100;
+            total2 = total2 + salary2;
+            System.out.println("Месяц " + s + ", сумма накоплений равна " + total2 + " рублей");
+        }
     }
-
 }
